09c37b53cbeb171869720857fdecc80684911ae8
JoseRenteria <jose.renteria@forwardlane.com>
2017-03-23 13:55:15 -0400

    added console.log for tracy

--- browser/js/people/people.factory.js
+++ browser/js/people/people.factory.js
@@ -1,6 +1,7 @@
 app.factory('peopleFactory', function($http) {
 
     function getData(response) {
+        console.log(response.data)
         return response.data;
     }
 
@@ -12,21 +13,25 @@ app.factory('peopleFactory', function($http) {
                 .catch()
         },
         getSinglePerson: function(personInfo) {
+            console.log('sending', personInfo)
             return $http.get('/api/people/' + personInfo.id)
                 .then(getData)
                 .catch()
         },
         postPerson: function(personInfo) {
+            console.log('sending', personInfo)
             return $http.post('/api/people', personInfo)
                 .then(getData)
                 .catch()
         },
         updatePerson: function(personInfo) {
+            console.log('sending', personInfo)
             return $http.put('/api/people/' + personInfo.id, personInfo)
                 .then(getData)
                 .catch()
         },
         deletePerson: function(personInfo) {
+            console.log('sending', personInfo)
             return $http.delete('/api/people/' + personInfo.id)
                 .then(getData)
                 .catch()
