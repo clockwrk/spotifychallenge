a391dadbde857944707388723d383d9cb173c192
JoseRenteria <jose.renteria@forwardlane.com>
2017-03-23 14:04:19 -0400

    updating reponse

--- browser/js/people/people.factory.js
+++ browser/js/people/people.factory.js
@@ -1,7 +1,7 @@
 app.factory('peopleFactory', function($http) {
 
     function getData(response) {
-        console.log(response.data)
+        console.log('response ',response.data)
         return response.data;
     }
 
@@ -13,25 +13,25 @@ app.factory('peopleFactory', function($http) {
                 .catch()
         },
         getSinglePerson: function(personInfo) {
-            console.log('sending', personInfo)
+            console.log('sending ', personInfo)
             return $http.get('/api/people/' + personInfo.id)
                 .then(getData)
                 .catch()
         },
         postPerson: function(personInfo) {
-            console.log('sending', personInfo)
+            console.log('sending ', personInfo)
             return $http.post('/api/people', personInfo)
                 .then(getData)
                 .catch()
         },
         updatePerson: function(personInfo) {
-            console.log('sending', personInfo)
+            console.log('sending ', personInfo)
             return $http.put('/api/people/' + personInfo.id, personInfo)
                 .then(getData)
                 .catch()
         },
         deletePerson: function(personInfo) {
-            console.log('sending', personInfo)
+            console.log('sending ', personInfo)
             return $http.delete('/api/people/' + personInfo.id)
                 .then(getData)
                 .catch()
